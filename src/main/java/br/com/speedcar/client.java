af4afa4e899f42815af5143f145faf291fea9c1e
Henrique Albrecht <henrique.jose@totvs.com.br>
2022-11-29 02:00:44 -0300

    Iniciação do projeto

---
+++ src/main/java/br/com/speedcar/client.java
@@ -0,0 +1,22 @@
+package br.com.speedcar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/client")
+public class client extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("<h1>Cadastro de clientes da SpeedCar Servlet</h1>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
